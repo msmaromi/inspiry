4c0e4b98ccf08783b5e83645c5c8ec1a4dae2b59
Muhammad S Maromi <mmaromi@alamisharia.co.id>
2021-02-04 10:31:47 +0700

    Change webhook url again

--- bin/inspiry.rb
+++ bin/inspiry.rb
@@ -8,7 +8,7 @@ QUOTES = [
   "\"The Quran is a friend, which will never let you down in this world and hereafter\"\n\nNgaji yuk squad @channel\nhttps://meet.google.com/ucx-orqs-nsh",
   "\"Don't leave me, don't stay away from me, keep me inside your heart, read me, understand me and obey me, *because tomorrow in the grave you will need me*\"\n\nNgaji yuk squad @channel\nhttps://meet.google.com/ucx-orqs-nsh"
 ]
-SLACK_WEBHOOK = "https://hooks.slack.com/services/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"
+SLACK_WEBHOOK = "https://hooks.slack.com/services/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"
 
 
 def get_quote
