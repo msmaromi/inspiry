00254c31e22dc2e8728e4b50f73e0319914c21a2
Muhammad S Maromi <mmaromi@alamisharia.co.id>
2021-02-04 13:04:54 +0700

    Fix for proper mention @channel

--- bin/inspiry.rb
+++ bin/inspiry.rb
@@ -3,10 +3,10 @@ require 'json'
 require 'date'
 
 QUOTES = [
-  "\"The more you read the Quran, the more you fall in love with The Author\"\n\nNgaji yuk squad @channel\nhttps://meet.google.com/ucx-orqs-nsh",
-  "\"Don't never ever be in your mind that you are the one who achieve something. Indeed Alloh make it easy for you.\"\n\nNgaji yuk squad @channel\nhttps://meet.google.com/ucx-orqs-nsh",
-  "\"The Quran is a friend, which will never let you down in this world and hereafter\"\n\nNgaji yuk squad @channel\nhttps://meet.google.com/ucx-orqs-nsh",
-  "\"Don't leave me, don't stay away from me, keep me inside your heart, read me, understand me and obey me, *because tomorrow in the grave you will need me*\"\n\nNgaji yuk squad @channel\nhttps://meet.google.com/ucx-orqs-nsh"
+  "\"The more you read the Quran, the more you fall in love with The Author\"\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/ucx-orqs-nsh",
+  "\"Don't never ever be in your mind that you are the one who achieve something. Indeed Alloh make it easy for you.\"\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/ucx-orqs-nsh",
+  "\"The Quran is a friend, which will never let you down in this world and hereafter\"\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/ucx-orqs-nsh",
+  "\"Don't leave me, don't stay away from me, keep me inside your heart, read me, understand me and obey me, *because tomorrow in the grave you will need me*\"\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/ucx-orqs-nsh"
 ]
 SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 
