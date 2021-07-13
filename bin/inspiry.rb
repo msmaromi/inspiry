09b2ebc20bd86cb3c3125cdbac0807393c0c1c5f
Muhammad S Maromi <mmaromi@alamisharia.co.id>
2021-07-14 04:59:37 +0700

    Using image quote instead of text

--- bin/inspiry.rb
+++ bin/inspiry.rb
@@ -2,37 +2,41 @@ require 'httparty'
 require 'json'
 require 'date'
 
-QUOTES = [
-  "The more you read the Quran, the more you fall in love with The Author.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Never ever be in your mind that you are the one who achieve something. Indeed Allah make it easy for you.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "The Quran is a friend, which will never let you down in this world and hereafter.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Don't leave me, don't stay away from me, keep me inside your heart, read me, understand me and obey me, *because tomorrow in the grave you will need me*.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Recharge your iman & boost your productivity? Read quran!\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Wordly life is very short, so turn to Allah before you return to Him.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Good days start with gratitude.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "And do not mix the truth with falsehood or conceal the truth while you know (it) - Baqarah 42.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Allah makes the impossible possible.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Call upon me, I will respond to you - Al ayah.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Indeed, Allah forgives All Sins - Al ayah.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "Do not despair of the mercy of Allah - Az zumar 53.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "HAPPINES is when Allah talks to you.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "HAPPINES is when Allah talks to you.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm",
-  "O you who believe! Seek help through patience and prayer. Indeed, Allah is with the patience. Al Baqarah 153.\n\nNgaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm"
+IMAGE_QUOTES = [
+  "https://i.pinimg.com/originals/c4/48/d3/c448d3ea718f8cd768514791c42358e8.jpg",
+  "https://i.pinimg.com/originals/7a/1e/d6/7a1ed6797aad86bff151b6114ccff48c.jpg",
+  "https://lh3.googleusercontent.com/proxy/sLcKFeUGRVJOE-wyewtJKSZYXGUqXoYIGmUXZwVb2NfB1MaS1vxJ0ERP_4o8maFdLZl264amkGm47tUcL6jKQbEpBez-z0Meg3uJERXZnScT4zNuWPmILOj-PJnPXUs5iH0yGQA_jxIAetJPCSJjiJ9XTLg94Np03OcT5yPAkA",
+  "https://lh3.googleusercontent.com/proxy/7UOzOq-_7IrQZkx-9oEJZqrRJt27huCIL0JJHGaUFSQfdz1rT-h8wYwKs5YWREAx0Ju8MCuIplMFHfYsXcmTKgOGap7qGQVFmzlNZFTJEFvbqtsaf-rMw6vKrPfdiTt5JIF-lX2MiJBCNX3LJ9AAMZBQiKDsefFSYTx6-VQNuQ",
+  "https://qph.fs.quoracdn.net/main-qimg-c3251b081ab538dd663226ce440a4f9f",
+  "https://thebrightquotes.com/wp-content/uploads/2020/11/Islamic-Inspirational-Quotes-on-Allah-43-1024x1024.jpg",
+  "https://cdn3.vectorstock.com/i/1000x1000/65/57/muslim-quote-and-saying-for-better-life-best-for-vector-23266557.jpg",
+  "https://cdn.hipwallpaper.com/i/11/85/9Tg7um.jpg",
+  "https://i.pinimg.com/originals/92/5c/eb/925ceb1fdbed7efb5e9b5feccbcff6ff.png",
+  "https://i.pinimg.com/originals/48/3f/26/483f263dd2fd0c884e62850ad2a2875d.jpg",
+  "https://i.pinimg.com/originals/ec/92/f6/ec92f65b12c619c158fb78003dff7dd2.jpg",
+  "https://i.pinimg.com/originals/ca/ce/db/cacedb9610c18860e5cc5fa5acc765ea.png",
+  "https://i.pinimg.com/originals/81/3a/10/813a103e44045344e813a42f04977e27.jpg",
+  "https://i.pinimg.com/736x/cd/bb/89/cdbb89e4bb04ce25a714916228186ced.jpg",
+  "https://i.pinimg.com/originals/de/df/44/dedf441dca98f12045be9c9d94ec8609.jpg",
+  "https://i.pinimg.com/originals/b0/cd/58/b0cd5811d17f4b8117b727f4bb869cfb.jpg",
+  "https://i.pinimg.com/736x/9c/f4/8e/9cf48ec42594230a5440bcdcee94b812.jpg"
 ]
+TEXT = "Ngaji yuk squad <!channel>\nhttps://meet.google.com/qgk-phfc-owm"
+CHANNEL_ID = "C017K819DJ8" # open slack via web to find the channel id
 SLACK_API_POST_MESSAGE = "https://slack.com/api/chat.postMessage"
 
 
 def get_quote
-  quote = QUOTES.sample
-  post_message(quote)
+  image_quote = IMAGE_QUOTES.sample
+  attachments = [{ :title => "Invitation", :image_url => image_quote }]
+  post_message(attachments)
 end
 
-def post_message quote  
+def post_message attachments
   puts "Transferring the vibe through Slack!"
-  puts quote  
   HTTParty.post(
-    SLACK_API_POST_MESSAGE, 
-    :query => {:channel => "C017K819DJ8", :text => quote, :pretty => 1},
+    SLACK_API_POST_MESSAGE,
+    :query => {:channel => CHANNEL_ID, :text => TEXT, :attachments => attachments.to_json, :pretty => 1},
     :headers => {'Authorization' => "Bearer #{ENV["SLACK_API_TOKEN"]}"}
   )
   puts "Posted a message. Hope they'd like it."
